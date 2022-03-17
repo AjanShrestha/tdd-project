eae6018e8405f7f0b8305d2bb5a017710fb235e5
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2022-03-18 08:24:34 +1100

    feat: improved error handling for missing exchange rates

--- py/portfolio.py
+++ py/portfolio.py
@@ -12,7 +12,7 @@ class Portfolio:
     def add(self, *moneys):
         self.moneys.extend(moneys)
 
-    def __convert(self, a_money: type[Money], a_currency: str) -> type[Money]:
+    def __convert(self, a_money: type[Money], a_currency: str) -> float:
         exchange_rates = {
             "EUR->USD": 1.2,
             "USD->KRW": 1100,
@@ -20,10 +20,19 @@ class Portfolio:
         if a_money.currency == a_currency:
             return a_money.amount
         key = f"{a_money.currency}->{a_currency}"
-        return a_money.amount * exchange_rates.get(key)
+        return a_money.amount * exchange_rates[key]
 
-    def evaluate(self, currency: str) -> type[Money]:
-        total = functools.reduce(
-            operator.add, map(lambda m: self.__convert(m, currency), self.moneys), 0
-        )
-        return Money(total, currency)
+    def evaluate(self, currency: str):
+        total = 0.0
+        failures = []
+        for m in self.moneys:
+            try:
+                total += self.__convert(m, currency)
+            except KeyError as ke:
+                failures.append(ke)
+
+        if not len(failures):
+            return Money(total, currency)
+
+        failure_message = ",".join(f.args[0] for f in failures)
+        raise Exception("Missing exchange rate(s):[{0}]".format(failure_message))
